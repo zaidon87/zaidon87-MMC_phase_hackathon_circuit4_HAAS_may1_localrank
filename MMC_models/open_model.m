236aa76674d88315ac0381e70e6b84421f605f8e
zaid Jabbar <zaidjabbar38@gmail.com>
2026-05-20 16:31:48 +0300

    Add MATLAB model opener

---
+++ MMC_models/open_model.m
@@ -0,0 +1,3 @@
+% Open the main MMC closed-loop NLM / local-rank model
+modelFile = fullfile(fileparts(mfilename('fullpath')), 'MMC_phase_hackathon_circuit4_HAAS_may1_localrank.slx');
+open_system(modelFile);
